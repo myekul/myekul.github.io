3e6f370c9f0b6c6925fbe4109ea76ac7770432d9
myekul <somebodynamedmichael@gmail.com>
2025-11-12 03:14:22 -0500

    Added info section

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+const allTabs = document.querySelectorAll('.tab')
+allTabs.forEach(elem => {
+    hide(elem)
+})
 setFooter('2025')
+setTabs(['home', 'info'])
+initializeHash('home')
+action()
 setResources()
 document.addEventListener('DOMContentLoaded', function () {
     show('bodyContent')
 })
 document.getElementById('runRecap').style.gap = 0
+function action() {
+    allTabs.forEach(tab => {
+        hide(tab)
+    })
+    switch (globalTab) {
+        case 'home':
+            show('homeTab')
+            break
+        case 'info':
+            show('infoTab')
+            break
+    }
+}
